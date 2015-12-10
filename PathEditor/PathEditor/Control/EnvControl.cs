b0d345fd40369ac1d03f8ce6eed0bdc5d459a7a2
ihuahua <qiao.hua@alibaba-inc.com>
2015-12-10 19:13:01 +0800

    实现EnvVar还原至系统

--- PathEditor/PathEditor/Control/EnvControl.cs
+++ PathEditor/PathEditor/Control/EnvControl.cs
@@ -18,14 +18,53 @@ namespace Huahua.Control
                     env.EnvGroups.Add(new EnvGroup(tag.ToString(), Environment.GetEnvironmentVariables(tag)));
                 }
             }
-            catch { }
+            catch
+            {
+                env = null;
+            }
 
             return env;
         }
 
         public static void SetSystemEnv(EnvVar env)
         {
-            throw new NotImplementedException("暂未实现");
+            if (env == null)
+                return;
+
+            foreach (var modifyGroup in env.EnvGroups)
+            {
+                if (modifyGroup.EnvUnits.Count <= 0)
+                    continue;
+
+                EnvironmentVariableTarget target;
+                if (!Enum.TryParse(modifyGroup.Target, out target))
+                    continue;
+
+                EnvGroup sysGroup = new EnvGroup(target.ToString(), Environment.GetEnvironmentVariables(target));
+
+                // 删除系统中不包含在Group里的变量
+                for (int uid = 0; uid < sysGroup.EnvUnits.Count; uid++)
+                {
+                    EnvUnit unit = sysGroup.EnvUnits[uid];
+                    if (!modifyGroup.EnvUnits.Contains(unit))
+                    {
+                        Environment.SetEnvironmentVariable(unit.Name, string.Empty, target);
+                        sysGroup.EnvUnits.Remove(unit);
+                        --uid;
+                    }
+                }
+
+                // 添加Group里的新变量
+                for (int uid = 0; uid < modifyGroup.EnvUnits.Count; uid++)
+                {
+                    EnvUnit unit = modifyGroup.EnvUnits[uid];
+                    if (!sysGroup.EnvUnits.Contains(unit))
+                    {
+                        Environment.SetEnvironmentVariable(unit.Name, unit.ToPathString(), target);
+                    }
+                }
+            }
+
         }
 
         public static EnvVar LoadEnvXml(string fn)
@@ -33,18 +72,21 @@ namespace Huahua.Control
             if (!File.Exists(fn))
                 return null;
 
-            EnvVar env = new EnvVar();
+            EnvVar env = null;
 
             try
             {
                 using (StreamReader reader = new StreamReader(fn))
                 {
-                    System.Xml.Serialization.XmlSerializer xmlSerializer = new System.Xml.Serialization.XmlSerializer(env.GetType());
+                    System.Xml.Serialization.XmlSerializer xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(EnvVar));
                     env = (EnvVar)xmlSerializer.Deserialize(reader);
                     reader.Close();
                 }
             }
-            catch { }
+            catch
+            {
+                env = null;
+            }
 
             return env;
         }
